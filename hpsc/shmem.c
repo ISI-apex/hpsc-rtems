dfa9ab31853c687362d6d7875be38b9c027bf324
Connor Imes <cimes@isi.edu>
2019-04-01 10:21:49 -0400

    hpsc: add shmem

---
+++ hpsc/shmem.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "shmem.h"
+
+// All subsystems must understand this structure and its protocol
+struct hpsc_shmem_region {
+    uint8_t data[SHMEM_MSG_SIZE];
+    uint32_t is_new;
+};
+
+struct shmem {
+    void *addr;
+};
+
+static volatile void *vmem_set(volatile void *s, int c, unsigned n)
+{
+    volatile uint8_t *bs = s;
+    while (n--)
+        *bs++ = (unsigned char) c;
+    return s;
+}
+
+static volatile void *vmem_cpy(volatile void *restrict dest, void *restrict src,
+                               unsigned n)
+{
+    // assume dest and src are word-aligned
+    volatile uint32_t *wd;
+    uint32_t *ws;
+    volatile uint8_t *bd;
+    uint8_t *bs;
+    for (wd = dest, ws = src; n >= sizeof(*wd); n -= sizeof(*wd))
+        *wd++ = *ws++;
+    for (bd = (uint8_t *) wd, bs = (uint8_t *) ws; n > 0; n--)
+        *bd++ = *bs++;
+    return dest;
+}
+
+static void *mem_vcpy(void *restrict dest, volatile void *restrict src,
+                      unsigned n)
+{
+    // assume dest and src are word-aligned
+    uint32_t *wd;
+    volatile uint32_t *ws;
+    uint8_t *bd;
+    volatile uint8_t *bs;
+    for (wd = dest, ws = src; n >= sizeof(*wd); n -= sizeof(*wd))
+        *wd++ = *ws++;
+    for (bd = (uint8_t *) wd, bs = (uint8_t *) ws; n > 0; n--)
+        *bd++ = *bs++;
+    return dest;
+}
+
+struct shmem *shmem_open(void *addr)
+{
+    struct shmem *s = malloc(sizeof(struct shmem));
+    if (s)
+        s->addr = addr;
+    return s;
+}
+
+void shmem_close(struct shmem *s)
+{
+    free(s);
+}
+
+size_t shmem_send(struct shmem *s, void *msg, size_t sz)
+{
+    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
+    size_t sz_rem = SHMEM_MSG_SIZE - sz;
+    assert(sz <= SHMEM_MSG_SIZE);
+    vmem_cpy(shm->data, msg, sz);
+    if (sz_rem)
+        vmem_set(shm->data + sz, 0, sz_rem);
+    shm->is_new = 1;
+    return sz;
+}
+
+uint32_t shmem_get_status(struct shmem *s)
+{
+    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
+    return shm->is_new;
+}
+
+size_t shmem_recv(struct shmem *s, void *msg, size_t sz)
+{
+    volatile struct hpsc_shmem_region *shm = (volatile struct hpsc_shmem_region *) s->addr;
+    assert(sz >= SHMEM_MSG_SIZE);
+    mem_vcpy(msg, shm->data, SHMEM_MSG_SIZE);
+    shm->is_new = 0;
+    return SHMEM_MSG_SIZE;
+}
---
+++ hpsc/shmem.h
@@ -0,0 +1,24 @@
+#ifndef SHMEM_H
+#define SHMEM_H
+
+#include <stdint.h>
+#include <unistd.h>
+
+// size aligns with mailbox messages
+#define SHMEM_MSG_SIZE 64
+
+struct shmem;
+
+struct shmem *shmem_open(void *addr);
+
+void shmem_close(struct shmem *s);
+
+// returns the number of bytes written
+size_t shmem_send(struct shmem *s, void *msg, size_t sz);
+
+uint32_t shmem_get_status(struct shmem *s);
+
+// returns 0the number of bytes read
+size_t shmem_recv(struct shmem *s, void *msg, size_t sz);
+
+#endif // SHMEM_H
