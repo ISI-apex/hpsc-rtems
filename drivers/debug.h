eb6a260cf3058f2e1e09d08e2f5a51f62eef8331
Connor Imes <cimes@isi.edu>
2019-04-01 11:10:13 -0400

    drivers: add debug

---
+++ drivers/debug.h
@@ -0,0 +1,15 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stdio.h>
+
+// Define DEBUG to 1 in the source file that you want to debug
+// before the #include statement for this header.
+#ifndef DEBUG
+#define DEBUG 0
+#endif // undef DEBUG
+
+#define DPRINTF(...) \
+        if (DEBUG) printf(__VA_ARGS__)
+
+#endif // DEBUG_H
