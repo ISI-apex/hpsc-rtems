b34b15c3871c8145686a8359004758fbda18b031
Connor Imes <cimes@isi.edu>
2019-04-05 12:49:18 -0400

    rtps-r52: tests: rti-timer: test all timers using correct affinity

--- rtps-r52/tests/rti-timer.c
+++ rtps-r52/tests/rti-timer.c
@@ -1,21 +1,59 @@
+#include <assert.h>
+#include <sched.h>
+#include <stdio.h>
+
 #include <rtems.h>
 
-// bist
+// drivers
+#include <hpsc-rti-timer.h>
+
+// selftest
 #include <hpsc-rti-timer-test.h>
+
+// libhpsc
+#include <affinity.h>
 
 // plat
 #include <hwinfo.h>
 
-#include "gic.h"
+#include "devices.h"
 #include "test.h"
 
 int test_core_rti_timer()
 {
-    rtems_vector_number vec = gic_irq_to_rvn(PPI_IRQ__RTI_TIMER,
-                                             GIC_IRQ_TYPE_PPI);
+    struct hpsc_rti_timer *tmr = NULL;
+    dev_id_cpu id = 0;
+    unsigned count = 0;
+    enum hpsc_rti_timer_test_rc rc = 0;
+    rtems_status_code sc;
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
 
-    // Test only one timer, because each timer can be tested only from
-    // its associated core, and this BM code is not SMP.
-    return hpsc_rti_timer_test(RTI_TIMER_RTPS_R52_0__RTPS_BASE, vec,
-                               RTI_MAX_COUNT);
+    printf("TEST: test_core_rti_timer: begin\n");
+
+    // store current affinity
+    sc = rtems_task_get_affinity(RTEMS_SELF, sizeof(cpuset), &cpuset);
+    assert(sc == RTEMS_SUCCESSFUL);
+    printf("Saving CPU affinity count: %d\n", CPU_COUNT(&cpuset));
+
+    dev_id_cpu_for_each_rtit(id, tmr) {
+        if (tmr) {
+            count++;
+            // must first bind to the correct CPU
+            printf("Setting CPU affinity to CPU: %u\n", id);
+            affinity_pin_self_to_cpu(id);
+            rc = hpsc_rti_timer_test_device(tmr, RTI_MAX_COUNT);
+            if (rc)
+                break;
+        }
+    }
+
+    // restore original affinity
+    printf("Restoring CPU affinity count: %d\n", CPU_COUNT(&cpuset));
+    sc = rtems_task_set_affinity(RTEMS_SELF, sizeof(cpuset), &cpuset);
+    assert(sc == RTEMS_SUCCESSFUL);
+
+    assert(count); // need to have tested at least one core's timer
+    printf("TEST: test_core_rti_timer: %s\n", rc ? "failed": "success");
+    return (int) rc;
 }
