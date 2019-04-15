42763a1a308e768591c7acf328e5918484682595
Connor Imes <cimes@isi.edu>
2019-04-15 12:02:14 -0400

    lib: hpsc: add hpsc-msg

---
+++ lib/hpsc/hpsc-msg.c
@@ -0,0 +1,61 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hpsc-msg.h"
+
+// info is for debugging, use real data types if we need more detail
+#define LIFECYCLE_INFO_SIZE (HPSC_MSG_PAYLOAD_SIZE - sizeof(uint32_t))
+struct hpsc_msg_lifeycle_payload {
+    uint32_t status;
+    char info[LIFECYCLE_INFO_SIZE];
+};
+
+static void msg_fill(uint8_t *buf, enum hpsc_msg_type t, const void *payload,
+                     size_t psz)
+{
+    assert(buf);
+    assert(psz <= HPSC_MSG_PAYLOAD_SIZE);
+    buf[0] = t;
+    if (payload)
+        memcpy(&buf[HPSC_MSG_PAYLOAD_OFFSET], payload, psz);
+}
+
+void hpsc_msg_wdt_timeout(void *buf, size_t sz, unsigned int cpu)
+{
+    assert(sz == HPSC_MSG_SIZE);
+    msg_fill(buf, WATCHDOG_TIMEOUT, &cpu, sizeof(cpu));
+}
+
+void hpsc_msg_lifecycle(void *buf, size_t sz,
+                        enum hpsc_msg_lifecycle_status status,
+                        const char *fmt, ...)
+{
+    // payload is the status enumeration and a string of debug data
+    va_list args;
+    struct hpsc_msg_lifeycle_payload p = {
+        .status = status,
+        .info = {0}
+    };
+    assert(sz == HPSC_MSG_SIZE);
+    if (fmt) {
+        va_start(args, fmt);
+        vsnprintf(p.info, LIFECYCLE_INFO_SIZE - 1, fmt, args);
+        va_end(args);
+    }
+    msg_fill(buf, LIFECYCLE, &p, sizeof(p));
+}
+
+void hpsc_msg_ping(void *buf, size_t sz)
+{
+    assert(sz == HPSC_MSG_SIZE);
+    msg_fill(buf, PING, NULL, 0);
+}
+
+void hpsc_msg_pong(void *buf, size_t sz)
+{
+    assert(sz == HPSC_MSG_SIZE);
+    msg_fill(buf, PONG, NULL, 0);
+}
---
+++ lib/hpsc/hpsc-msg.h
@@ -0,0 +1,54 @@
+#ifndef HPSC_MSG_H
+#define HPSC_MSG_H
+
+#include <stdint.h>
+
+#define HPSC_MSG_SIZE 64
+#define HPSC_MSG_PAYLOAD_OFFSET 4
+#define HPSC_MSG_PAYLOAD_SIZE (HPSC_MSG_SIZE - 4)
+
+#define HPSC_MSG_DEFINE(name) uint8_t name[HPSC_MSG_SIZE] = { 0 }
+
+// Message type enumeration
+enum hpsc_msg_type {
+    // Value 0 is reserved so empty messages can be recognized
+    NOP = 0,
+    // test messages
+    PING,
+    PONG,
+    // responses - payload contains ID of the response being acknowledged
+    READ_VALUE,
+    WRITE_STATUS,
+    // general operations
+    READ_FILE,
+    WRITE_FILE,
+    READ_PROP,
+    WRITE_PROP,
+    READ_ADDR,
+    WRITE_ADDR,
+    // notifications
+    WATCHDOG_TIMEOUT,
+    FAULT,
+    LIFECYCLE,
+    // an enumerated/predefined action
+    ACTION,
+    // enum counter
+    HPSC_MSG_TYPE_COUNT
+};
+
+enum hpsc_msg_lifecycle_status {
+    LIFECYCLE_UP,
+    LIFECYCLE_DOWN
+};
+
+void hpsc_msg_wdt_timeout(void *buf, size_t sz, unsigned int cpu);
+
+void hpsc_msg_lifecycle(void *buf, size_t sz,
+                        enum hpsc_msg_lifecycle_status status,
+                        const char *fmt, ...);
+
+void hpsc_msg_ping(void *buf, size_t sz);
+
+void hpsc_msg_pong(void *buf, size_t sz);
+
+#endif // HPSC_MSG_H
