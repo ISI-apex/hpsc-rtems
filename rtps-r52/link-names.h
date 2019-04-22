a2b8d8bb49fc044e4487db633429e4a57ab3119b
Connor Imes <cimes@isi.edu>
2019-04-22 09:46:22 -0400

    rtps-r52: add link-names.h

---
+++ rtps-r52/link-names.h
@@ -0,0 +1,24 @@
+#ifndef LINK_NAMES_H
+#define LINK_NAMES_H
+
+/*
+ * Format is: LINK_NAME__<IFACE>__<REMOTE>_<DIRECTION>
+ *
+ * For example:
+ * LINK_NAME__MBOX__TRCH_SERVER is the local server link for handling mailbox
+ * requests from TRCH.
+ * LINK_NAME__MBOX__TRCH_CLIENT is the local client link for sending mailbox
+ * requests to TRCH.
+ */
+
+// Connections to/from TRCH
+
+#define LINK_NAME__MBOX__TRCH_SERVER "LINK_MBOX_TRCH_SERVER"
+#define LINK_NAME__MBOX__TRCH_CLIENT "LINK_MBOX_TRCH_CLIENT"
+
+// Connections to/from HPPS
+
+#define LINK_NAME__MBOX__HPPS_SERVER "LINK_MBOX_HPPS_SERVER"
+#define LINK_NAME__MBOX__HPPS_CLIENT "LINK_MBOX_HPPS_CLIENT"
+
+#endif // LINK_NAMES_H
