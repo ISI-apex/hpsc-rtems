af2c3897fc22e8f6ee30c1a8a293f55becb3f5f6
Connor Imes <cimes@isi.edu>
2019-04-25 10:59:47 -0400

    lib: hpsc: command: add cmd_drop_all; some internal refactoring

--- lib/hpsc/command.c
+++ lib/hpsc/command.c
@@ -9,21 +9,14 @@
 #include <rtems/bspIo.h>
 
 #include "command.h"
+#include "link.h"
 #include "hpsc-msg.h"
 
 #define CMDQ_LEN 4
 
-struct cmdq_item {
-    struct cmd cmd;
+struct cmd_handled_ctx {
     cmd_handled_t *cb;
     void *cb_arg;
-};
-
-struct cmdq {
-    struct cmdq_item q[CMDQ_LEN];
-    pthread_spinlock_t lock; // no init is necessary
-    size_t head;
-    size_t tail;
 };
 
 struct cmd_handler_ctx {
@@ -33,9 +26,16 @@ struct cmd_handler_ctx {
     bool running;
 };
 
-struct cmd_handled_ctx {
-    cmd_handled_t *cb;
-    void *cb_arg;
+struct cmdq_item {
+    struct cmd cmd;
+    struct cmd_handled_ctx handled;
+};
+
+struct cmdq {
+    struct cmdq_item q[CMDQ_LEN];
+    pthread_spinlock_t lock; // no init is necessary
+    size_t head;
+    size_t tail;
 };
 
 static struct cmdq cmdq = { 0 };
@@ -54,84 +54,60 @@ static struct cmd_handled_ctx cmd_handled = {
 };
 
 
-void cmd_handled_register_cb(cmd_handled_t *cb, void *cb_arg)
+static void lock(pthread_spinlock_t *lock)
 {
-    cmd_handled.cb = cb;
-    cmd_handled.cb_arg = cb_arg;
-}
-
-void cmd_handled_unregister_cb(void)
-{
-    cmd_handled.cb = NULL;
-    cmd_handled.cb_arg = NULL;
-}
-
-int cmd_enqueue_cb(struct cmd *cmd, cmd_handled_t *cb, void *cb_arg)
-{
-    int err;
-    int rc = 0;
-    rtems_id tid;
-    assert(cmd);
-
-    err = pthread_spin_lock(&cmdq.lock);
+    int err = pthread_spin_lock(lock);
     assert(!err);
+}
+
+static void unlock(pthread_spinlock_t *lock)
+{
+    int err = pthread_spin_unlock(lock);
+    assert(!err);
+}
+
+static size_t cmdq_size_unsafe(void)
+{
+    return (cmdq.head >= cmdq.tail) ? (cmdq.head - cmdq.tail) :
+        (CMDQ_LEN - cmdq.tail + cmdq.head);
+}
+
+static int cmd_enqueue_cb_unsafe(struct cmd *cmd, cmd_handled_t *cb,
+                                 void *cb_arg)
+{
+    assert(cmd);
     if (cmdq.head + 1 % CMDQ_LEN == cmdq.tail) {
         printk("command: enqueue failed: queue full\n");
-        rc = 1;
-        goto out;
+        return 1;
     }
     cmdq.head = (cmdq.head + 1) % CMDQ_LEN;
-
     memcpy(&cmdq.q[cmdq.head].cmd, cmd, sizeof(struct cmd));
-    cmdq.q[cmdq.head].cb = cb;
-    cmdq.q[cmdq.head].cb_arg = cb_arg;
+    cmdq.q[cmdq.head].handled.cb = cb;
+    cmdq.q[cmdq.head].handled.cb_arg = cb_arg;
     printk("command: enqueue (tail %u head %u): cmd %u arg %u...\n",
            cmdq.tail, cmdq.head,
            cmdq.q[cmdq.head].cmd.msg[0],
            cmdq.q[cmdq.head].cmd.msg[HPSC_MSG_PAYLOAD_OFFSET]);
-
-out:
-    err = pthread_spin_unlock(&cmdq.lock);
-    assert(!err);
-    tid = cmd_handler.tid;
-    if (!rc && tid != RTEMS_ID_NONE)
-        rc = rtems_event_send(tid, RTEMS_EVENT_0) != RTEMS_SUCCESSFUL;
-    return rc;
+    return 0;
 }
 
-int cmd_enqueue(struct cmd *cmd)
+static int cmd_dequeue_unsafe(struct cmd *cmd, cmd_handled_t **cb,
+                              void **cb_arg)
 {
-    return cmd_enqueue_cb(cmd, NULL, NULL);
-}
-
-static int cmd_dequeue(struct cmd *cmd, cmd_handled_t **cb, void **cb_arg)
-{
-    int err;
-    int rc = 0;
     assert(cmd);
     assert(cb);
     assert(cb_arg);
-
-    err = pthread_spin_lock(&cmdq.lock);
-    assert(!err);
-    if (cmdq.head == cmdq.tail) {
-        rc = 1;
-        goto out;
-    }
+    if (cmdq.head == cmdq.tail)
+        return 1;
     cmdq.tail = (cmdq.tail + 1) % CMDQ_LEN;
-
     memcpy(cmd, &cmdq.q[cmdq.tail].cmd, sizeof(struct cmd));
-    *cb = cmdq.q[cmdq.tail].cb;
-    *cb_arg = cmdq.q[cmdq.tail].cb_arg;
+    *cb = cmdq.q[cmdq.tail].handled.cb;
+    *cb_arg = cmdq.q[cmdq.tail].handled.cb_arg;
     printk("command: dequeue (tail %u head %u): cmd %u arg %u...\n",
            cmdq.tail, cmdq.head,
            cmdq.q[cmdq.tail].cmd.msg[0],
            cmdq.q[cmdq.tail].cmd.msg[HPSC_MSG_PAYLOAD_OFFSET]);
-
-out:
-    err = pthread_spin_unlock(&cmdq.lock);
-    assert(!err);
-    return rc;
+    return 0;
 }
 
 static void cmd_handle(struct cmd *cmd, cmd_handled_t *cb, void *cb_arg)
@@ -173,19 +149,73 @@ out:
         cmd_handled.cb(cmd_handled.cb_arg, status);
 }
 
-static rtems_task cmd_handle_task(rtems_task_argument ignored)
+void cmd_handled_register_cb(cmd_handled_t *cb, void *cb_arg)
+{
+    cmd_handled.cb = cb;
+    cmd_handled.cb_arg = cb_arg;
+}
+
+void cmd_handled_unregister_cb(void)
+{
+    cmd_handled.cb = NULL;
+    cmd_handled.cb_arg = NULL;
+}
+
+int cmd_enqueue_cb(struct cmd *cmd, cmd_handled_t *cb, void *cb_arg)
+{
+    int rc;
+    lock(&cmdq.lock);
+    rc = cmd_enqueue_cb_unsafe(cmd, cb, cb_arg);
+    unlock(&cmdq.lock);
+    if (!rc && cmd_handler.tid != RTEMS_ID_NONE)
+        rc = rtems_event_send(cmd_handler.tid, RTEMS_EVENT_0) != RTEMS_SUCCESSFUL;
+    return rc;
+}
+
+int cmd_enqueue(struct cmd *cmd)
+{
+    return cmd_enqueue_cb(cmd, NULL, NULL);
+}
+
+static int cmd_dequeue(struct cmd *cmd, cmd_handled_t **cb, void **cb_arg)
+{
+    int rc;
+    lock(&cmdq.lock);
+    rc = cmd_dequeue_unsafe(cmd, cb, cb_arg);
+    unlock(&cmdq.lock);
+    return rc;
+}
+
+static size_t cmd_flush(void)
 {
     struct cmd cmd;
     cmd_handled_t *cb;
     void *cb_arg;
+    size_t i = 0;
+    while (!cmd_dequeue(&cmd, &cb, &cb_arg)) {
+        cmd_handle(&cmd, cb, cb_arg);
+        i++;
+    }
+    return i;
+}
+
+size_t cmd_drop_all(void)
+{
+    size_t qsize;
+    lock(&cmdq.lock);
+    qsize = cmdq_size_unsafe();
+    cmdq.tail = cmdq.head;
+    unlock(&cmdq.lock);
+    return qsize;
+}
+
+static rtems_task cmd_handle_task(rtems_task_argument ignored)
+{
     rtems_event_set events;
-    unsigned i = 0;
+    size_t i = 0;
     while (1) {
-        printk("[%u] Waiting for command...\n", i);
-        while (!cmd_dequeue(&cmd, &cb, &cb_arg)) {
-            cmd_handle(&cmd, cb, cb_arg);
-            i++;
-        }
+        printk("[%zu] Waiting for command...\n", i);
+        i += cmd_flush();
         events = 0;
         // RTEMS_EVENT_0: process a command
         // RTEMS_EVENT_1: exit handler task
--- lib/hpsc/command.h
+++ lib/hpsc/command.h
@@ -25,14 +25,47 @@ struct cmd {
 typedef ssize_t (cmd_handler_t)(struct cmd *cmd, void *reply, size_t reply_sz);
 typedef void (cmd_handled_t)(void *arg, cmd_status status);
 
+/**
+ * Register a callback handler to be run after every queued command is handled.
+ * This operation is not synchronized with cmd_handle_task, so don't use after
+ * cmd_handle_task is started unless the command queue is empty and guaranteed 
+ * not to enqueue new commands until operation completes.
+ */
 void cmd_handled_register_cb(cmd_handled_t *cb, void *cb_arg);
+
+/**
+ * Unregister the callback handler.
+ * This operation is not synchronized with cmd_handle_task, so don't use after
+ * cmd_handle_task is started unless the command queue is empty and guaranteed 
+ * not to enqueue new commands until operation completes.
+ */
 void cmd_handled_unregister_cb(void);
 
+/**
+ * Enqueue a command with its own callback handler (cmd struct will be copied).
+ * The specified callback handler will be executed before the global handler.
+ */
 int cmd_enqueue_cb(struct cmd *cmd, cmd_handled_t *cb, void *cb_arg);
+
+/**
+ * Enqueue a command (cmd struct will be copied).
+ */
 int cmd_enqueue(struct cmd *cmd);
 
+/**
+ * Drop all commands in the queue without handling them.
+ */
+size_t cmd_drop_all(void);
+
+/**
+ * Start the cmd_handle_task (task must be created by the caller).
+ */
 rtems_status_code cmd_handle_task_start(rtems_id task_id, cmd_handler_t cb,
                                         rtems_interval timeout_ticks);
+
+/**
+ * Stop the cmd_handle_task.
+ */
 rtems_status_code cmd_handle_task_destroy(void);
 
 #endif // COMMAND_H
