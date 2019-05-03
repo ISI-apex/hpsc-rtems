e914e87c54fc785908681b1eecd8246a07f211b8
Connor Imes <cimes@isi.edu>
2019-05-03 10:42:00 -0400

    lib: hpsc: affinity: document interface

--- lib/hpsc/affinity.h
+++ lib/hpsc/affinity.h
@@ -3,8 +3,21 @@
 
 #include <rtems.h>
 
+/**
+ * Set provided task ID's affinity to use only the provided CPU.
+ *
+ * @retval RTEMS_SUCCESSFUL Successful operation.
+ * @retval RTEMS_INVALID_ID Invalid task identifier.
+ *
+ * @see rtems_task_set_affinity()
+ */
 rtems_status_code affinity_pin_to_cpu(rtems_id task_id, int cpu);
 
+/**
+ * Set the current task's affinity to use only the provided CPU.
+ *
+ * @see rtems_task_set_affinity()
+ */
 void affinity_pin_self_to_cpu(int cpu);
 
 #endif // AFFINITY_H
