26ca85fe36d3c781f56eea3c974ac2f53b3622a4
Connor Imes <cimes@isi.edu>
2019-09-03 12:57:39 -0400

    rtps-r52: add shell-tests

---
+++ rtps-r52/shell-tests.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <rtems.h>
+#include <rtems/shell.h>
+
+#include "test.h"
+
+#define SHELL_TESTS_TOPIC "hpsc-rtps-r52-test"
+
+/******************************************************************************/
+// Standalone
+/******************************************************************************/
+
+static int shell_test_command(int argc RTEMS_UNUSED, char *argv[] RTEMS_UNUSED)
+{
+    return test_command();
+}
+rtems_shell_cmd_t shell_cmd_test_command = {
+    "test_command",                            /* name */
+    "test_command",                            /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_command,                        /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_cpu_rti_timers(int argc RTEMS_UNUSED,
+                                     char *argv[] RTEMS_UNUSED)
+{
+#if CONFIG_RTI_TIMER
+    return test_cpu_rti_timers();
+#else 
+    fprintf(stderr, "ERROR: CONFIG_RTI_TIMER is not set!\n")
+    return -1;
+#endif // CONFIG_RTI_TIMER
+}
+rtems_shell_cmd_t shell_cmd_test_cpu_rti_timers = {
+    "test_cpu_rti_timers",                     /* name */
+    "test_cpu_rti_timers",                     /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_cpu_rti_timers,                 /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_lsio_sram_syscfg(int argc RTEMS_UNUSED,
+                                       char *argv[] RTEMS_UNUSED)
+{
+    return test_lsio_sram_syscfg();
+}
+rtems_shell_cmd_t shell_cmd_test_lsio_sram_syscfg = {
+    "test_lsio_sram_syscfg",                   /* name */
+    "test_lsio_sram_syscfg",                   /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_lsio_sram_syscfg,               /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_lsio_sram_dma_syscfg(int argc RTEMS_UNUSED,
+                                           char *argv[] RTEMS_UNUSED)
+{
+    return test_lsio_sram_dma_syscfg();
+}
+rtems_shell_cmd_t shell_cmd_test_lsio_sram_dma_syscfg = {
+    "test_lsio_sram_dma_syscfg",               /* name */
+    "test_lsio_sram_dma_syscfg",               /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_lsio_sram_dma_syscfg,           /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_mbox_lsio_loopback(int argc RTEMS_UNUSED,
+                                         char *argv[] RTEMS_UNUSED)
+{
+#if CONFIG_MBOX_LSIO
+    return test_mbox_lsio_loopback();
+#else
+    fprintf(stderr, "ERROR: CONFIG_MBOX_LSIO is not set!\n");
+    return -1;
+#endif // CONFIG_MBOX_LSIO
+}
+rtems_shell_cmd_t shell_cmd_test_mbox_lsio_loopback = {
+    "test_mbox_lsio_loopback",                 /* name */
+    "test_mbox_lsio_loopback",                 /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_mbox_lsio_loopback,             /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_rtps_mmu(int argc RTEMS_UNUSED, char *argv[] RTEMS_UNUSED)
+{
+    return test_rtps_mmu(false);
+}
+rtems_shell_cmd_t shell_cmd_test_rtps_mmu = {
+    "test_rtps_mmu",                           /* name */
+    "test_rtps_mmu",                           /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_rtps_mmu,                       /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_rtps_mmu_dma(int argc RTEMS_UNUSED,
+                                   char *argv[] RTEMS_UNUSED)
+{
+    return test_rtps_mmu(true);
+}
+rtems_shell_cmd_t shell_cmd_test_rtps_mmu_dma = {
+    "test_rtps_mmu_dma",                       /* name */
+    "test_rtps_mmu_dma",                       /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_rtps_mmu_dma,                   /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_shmem(int argc RTEMS_UNUSED, char *argv[] RTEMS_UNUSED)
+{
+    return test_shmem();
+}
+rtems_shell_cmd_t shell_cmd_test_shmem = {
+    "test_shmem",                              /* name */
+    "test_shmem",                              /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_shmem,                          /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+/******************************************************************************/
+// Local runtime
+/******************************************************************************/
+
+static int shell_test_command_server(int argc RTEMS_UNUSED,
+                                     char *argv[] RTEMS_UNUSED)
+{
+    return test_command_server();
+}
+rtems_shell_cmd_t shell_cmd_test_command_server = {
+    "test_command_server",                     /* name */
+    "test_command_server",                     /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_command_server,                 /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_link_shmem(int argc RTEMS_UNUSED,
+                                 char *argv[] RTEMS_UNUSED)
+{
+    return test_link_shmem();
+}
+rtems_shell_cmd_t shell_cmd_test_link_shmem = {
+    "test_link_shmem",                         /* name */
+    "test_link_shmem",                         /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_link_shmem,                     /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+/******************************************************************************/
+// Externally dependent
+/******************************************************************************/
+
+static int shell_test_link_mbox_trch(int argc RTEMS_UNUSED,
+                                     char *argv[] RTEMS_UNUSED)
+{
+#if CONFIG_LINK_MBOX_TRCH_CLIENT
+    return test_link_mbox_trch();
+#else
+    fprintf(stderr, "ERROR: CONFIG_LINK_MBOX_TRCH_CLIENT is not set!\n");
+    return -1;
+#endif // CONFIG_LINK_MBOX_TRCH_CLIENT
+}
+rtems_shell_cmd_t shell_cmd_test_link_mbox_trch = {
+    "test_link_mbox_trch",                     /* name */
+    "test_link_mbox_trch",                     /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_link_mbox_trch,                 /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
+
+static int shell_test_link_shmem_trch(int argc RTEMS_UNUSED,
+                                      char *argv[] RTEMS_UNUSED)
+{
+#if CONFIG_LINK_SHMEM_TRCH_CLIENT
+    return test_link_shmem_trch();
+#else
+    fprintf(stderr, "ERROR: CONFIG_LINK_SHMEM_TRCH_CLIENT is not set!\n");
+    return -1;
+#endif // CONFIG_LINK_SHMEM_TRCH_CLIENT
+}
+rtems_shell_cmd_t shell_cmd_test_link_shmem_trch = {
+    "test_link_shmem_trch",                    /* name */
+    "test_link_shmem_trch",                    /* usage */
+    SHELL_TESTS_TOPIC,                         /* topic */
+    shell_test_link_shmem_trch,                /* command */
+    NULL, NULL,                                /* alias, next */
+    0, 0, 0                                    /* mode, uid, gid */
+};
---
+++ rtps-r52/shell-tests.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_TESTS_H
+#define SHELL_TESTS_H
+
+#include <rtems.h>
+#include <rtems/shell.h>
+
+// Shell test commands wrap functions in test.h
+
+// Standalone
+extern rtems_shell_cmd_t shell_cmd_test_command;
+extern rtems_shell_cmd_t shell_cmd_test_cpu_rti_timers;
+extern rtems_shell_cmd_t shell_cmd_test_lsio_sram_syscfg;
+extern rtems_shell_cmd_t shell_cmd_test_lsio_sram_dma_syscfg;
+extern rtems_shell_cmd_t shell_cmd_test_mbox_lsio_loopback;
+extern rtems_shell_cmd_t shell_cmd_test_rtps_mmu;
+extern rtems_shell_cmd_t shell_cmd_test_rtps_mmu_dma;
+extern rtems_shell_cmd_t shell_cmd_test_shmem;
+
+// Local runtime
+extern rtems_shell_cmd_t shell_cmd_test_command_server;
+extern rtems_shell_cmd_t shell_cmd_test_link_shmem;
+
+// Externally dependent
+extern rtems_shell_cmd_t shell_cmd_test_link_mbox_trch;
+extern rtems_shell_cmd_t shell_cmd_test_link_shmem_trch;
+
+#endif // SHELL_TESTS_H
