0d100b688d48d32694b18913b7b4a27a185ac0b3
Connor Imes <cimes@isi.edu>
2019-09-05 08:51:28 -0400

    lib: drivers: hpsc-rti-timer: use base struct instead of regops

--- lib/drivers/hpsc-rti-timer.c
+++ lib/drivers/hpsc-rti-timer.c
@@ -1,5 +1,3 @@
-#define DEBUG 0
-
 #include <assert.h>
 #include <inttypes.h>
 #include <stdlib.h>
@@ -10,16 +8,27 @@
 
 #include "gic-trigger.h"
 #include "hpsc-rti-timer.h"
-#include "regops.h"
+
+#ifdef HPSC_RTIT_DEBUG
+#define HPSC_RTIT_DBG(...) printk(__VA_ARGS__)
+#else
+#define HPSC_RTIT_DBG(...)
+#endif
 
 
-#define REG__INTERVAL_LO    0x00
-#define REG__INTERVAL_HI    0x04
-#define REG__COUNT_LO       0x08
-#define REG__COUNT_HI       0x0c
+struct hpsc_rtit_base {
+    uint64_t INTERVAL;
+    uint64_t COUNT;
+    uint32_t CMD_ARM;
+    uint32_t CMD_FIRE;
+};
 
-#define REG__CMD_ARM        0x10
-#define REG__CMD_FIRE       0x14
+struct hpsc_rti_timer {
+    volatile struct hpsc_rtit_base *base;
+    const char *name;
+    rtems_vector_number vec;
+    bool is_started;
+};
 
 enum cmd {
     CMD_CAPTURE = 0,
@@ -32,26 +41,15 @@ struct cmd_code {
     uint32_t fire;
 };
 
-// cmd -> (arm, fire)
 static const struct cmd_code cmd_codes[] = {
     [CMD_CAPTURE] = { 0xCD01, 0x01CD },
     [CMD_LOAD]    = { 0xCD02, 0x02CD },
 };
 
-struct hpsc_rti_timer {
-    uintptr_t base;
-    const char *name;
-    rtems_vector_number vec;
-    bool is_started;
-};
-
 static void exec_cmd(struct hpsc_rti_timer *tmr, enum cmd cmd)
 {
-    // TODO: Arm-Fire is no protection from a stray jump over here...
-    // Are we supposed to separate these with data-driven control flow?
-    const struct cmd_code *code = &cmd_codes[cmd];
-    REGB_WRITE32(tmr->base, REG__CMD_ARM,  code->arm);
-    REGB_WRITE32(tmr->base, REG__CMD_FIRE, code->fire);
+    tmr->base->CMD_ARM = cmd_codes[cmd].arm;
+    tmr->base->CMD_FIRE = cmd_codes[cmd].fire;
 }
 
 static void hpsc_rti_timer_init(
@@ -60,7 +58,7 @@ static void hpsc_rti_timer_init(
     uintptr_t base,
     rtems_vector_number vec)
 {
-    tmr->base = base;
+    tmr->base = (volatile struct hpsc_rtit_base *)base;
     tmr->name = name;
     tmr->vec = vec;
     tmr->is_started = false;
@@ -73,9 +71,9 @@ rtems_status_code hpsc_rti_timer_probe(
     rtems_vector_number vec
 )
 {
-    printk("RTI TMR: %s: probe\n", name);
-    printk("\tbase: 0x%"PRIxPTR"\n", base);
-    printk("\tvec: %u\n", vec);
+    HPSC_RTIT_DBG("RTIT: %s: probe\n", name);
+    HPSC_RTIT_DBG("\tbase: 0x%"PRIxPTR"\n", base);
+    HPSC_RTIT_DBG("\tvec: %u\n", vec);
     *tmr = malloc(sizeof(struct hpsc_rti_timer));
     if (!*tmr)
         return RTEMS_NO_MEMORY;
@@ -88,7 +86,7 @@ rtems_status_code hpsc_rti_timer_probe(
 rtems_status_code hpsc_rti_timer_remove(struct hpsc_rti_timer *tmr)
 {
     assert(tmr);
-    printk("RTI TMR: %s: remove\n", tmr->name);
+    HPSC_RTIT_DBG("RTIT: %s: remove\n", tmr->name);
     if (tmr->is_started)
         return RTEMS_RESOURCE_IN_USE;
     // reset vector to level-sensitive
@@ -105,13 +103,11 @@ rtems_status_code hpsc_rti_timer_start(
 {
     rtems_status_code sc;
     assert(tmr);
-    DPRINTK("RTI TMR: %s: start\n", tmr->name);
+    HPSC_RTIT_DBG("RTIT: %s: start\n", tmr->name);
     sc = rtems_interrupt_handler_install(tmr->vec, tmr->name,
                                          RTEMS_INTERRUPT_UNIQUE, handler, arg);
     if (sc == RTEMS_SUCCESSFUL)
         tmr->is_started = true;
-    else
-        printk("RTI TMR: %s: failed to install interrupt handler\n", tmr->name);
     return sc;
 }
 
@@ -123,32 +119,29 @@ rtems_status_code hpsc_rti_timer_stop(
 {
     rtems_status_code sc;
     assert(tmr);
-    DPRINTK("RTI TMR: %s: stop\n", tmr->name);
+    HPSC_RTIT_DBG("RTIT: %s: stop\n", tmr->name);
     sc = rtems_interrupt_handler_remove(tmr->vec, handler, arg);
     if (sc == RTEMS_SUCCESSFUL)
         tmr->is_started = false;
-    else
-        printk("RTI TMR: %s: failed to remove interrupt handler\n", tmr->name);
     return sc;
 }
 
 uint64_t hpsc_rti_timer_capture(struct hpsc_rti_timer *tmr)
 {
+    uint64_t count;
     assert(tmr);
     exec_cmd(tmr, CMD_CAPTURE);
-    uint64_t count = ((uint64_t)REGB_READ32(tmr->base, REG__COUNT_HI) << 32) |
-                     REGB_READ32(tmr->base, REG__COUNT_LO);
-    DPRINTK("RTI TMR: %s: capture: count -> 0x%08x%08x\n", tmr->name,
-            (uint32_t)(count >> 32), (uint32_t)count);
+    count = tmr->base->COUNT;
+    HPSC_RTIT_DBG("RTIT: %s: capture: count -> 0x%016"PRIx64"\n",
+                  tmr->name, count);
     return count;
 }
 
 void hpsc_rti_timer_configure(struct hpsc_rti_timer *tmr, uint64_t interval)
 {
     assert(tmr);
-    REGB_WRITE32(tmr->base, REG__INTERVAL_HI, interval >> 32);
-    REGB_WRITE32(tmr->base, REG__INTERVAL_LO, interval & 0xffffffff);
-    DPRINTK("RTI TMR: %s: configure: interval <- 0x%08x%08x\n", tmr->name,
-            (uint32_t)(interval >> 32), (uint32_t)interval);
+    HPSC_RTIT_DBG("RTIT: %s: configure: interval <- 0x%016"PRIx64"\n",
+                  tmr->name, interval);
+    tmr->base->INTERVAL = interval;
     exec_cmd(tmr, CMD_LOAD);
 }
