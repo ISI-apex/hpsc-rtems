64b3679d3cbb1b587b39e58b22dfa46af2eefff6
Alexei Colin <acolin@isi.edu>
2020-03-14 19:32:52 -0400

    rtps-r52,plat: update mem-map to hpsc-baremetal 968e69a

--- plat/mem-map.h
+++ plat/mem-map.h
@@ -11,10 +11,8 @@
 // ID - some useful identifier for what's in memory
 //
 // Shared memory messaging: ID = 'SHM'__SRC_SW__DEST_SW
-// SRC, DST: [TRCH, RTPS_R52_LOCKSTEP, RTPS_R52_SMP,
-//            RTPS_R52_SPLIT_0, RTPS_R52_SPLIT_1, RTPS_A53,
-//            HPPS_SMP, HPPS_SMP_CL0, HPPS_SMP_CL1] - subsystem
-// SW: [SSW, ATF, APP] - the SRC/DST software component
+// SRC, DST: subsystem (enum sw_subsys in subsys.h)
+// SW: the software component on SRC/DEST subsystem (enum sw_comp in susbys.h)
 
 // TODO: Update remaining macros to use the naming convention
 
@@ -22,79 +20,108 @@
 // RTPS //
 //////////
 
-// Binary images with executables for RTPS
+// Used by RTEMS
 #define RTPS_DDR_ADDR__RTPS_R52_LOCKSTEP               0x40000000
 #define RTPS_DDR_SIZE__RTPS_R52_LOCKSTEP               0x7e000000 // excludes shm
 
-// Page tables for RTPS MMU
-#define RTPS_PT_ADDR 0x40010000
+// Page tables for RTPS MMU: in RPTS DRAM
+#define RTPS_PT_ADDR 0x40060000
 #define RTPS_PT_SIZE   0x200000
 
-// DMA Microcode buffer (cannot be in TCM)
-#define RTPS_DMA_MCODE_ADDR 0x40000000
+// DMA Microcode buffer: in RTPS DRAM (cannot be in TCM)
+#define RTPS_DMA_MCODE_ADDR 0x40050000
 #define RTPS_DMA_MCODE_SIZE 0x00001000
 
-// Buffers for DMA test
-#define RTPS_DMA_SRC_ADDR       0x40001000
+// Buffers for DMA test: in RTPS DRAM
+#define RTPS_DMA_SRC_ADDR       0x40051000
 #define RTPS_DMA_SIZE           0x00000200
-#define RTPS_DMA_DST_ADDR       0x40002000 // align to page
-#define RTPS_DMA_DST_REMAP_ADDR 0x40003000 // MMU test maps this to DST_ADDR
 
-// Shared memory regions accessible to RTPS - kept at end of RTPS DRAM region
-#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP                   0xbe000000
-#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP                   0x02000000
-// TRCH <-> RTPS R52
-#define RTPS_DDR_ADDR__SHM__TRCH_SSW__RTPS_R52_LOCKSTEP_SSW     0xbe000000
-#define RTPS_DDR_SIZE__SHM__TRCH_SSW__RTPS_R52_LOCKSTEP_SSW     0x00008000
-#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP_SSW__TRCH_SSW     0xbe008000
-#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP_SSW__TRCH_SSW     0x00008000
-// Shared memory - reserved but not allocated
-#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP__FREE             0xbe010000
-#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP__FREE             0x01ff0000
+#define RTPS_DMA_DST_ADDR       0x40052000 // align to page
+#define RTPS_DMA_DST_REMAP_ADDR 0x40053000 // MMU test maps this to DST_ADDR
+
+/* Allocations can overlap for subsystems that cannot run concurrently. */
+/* Note: SMP OS uses one region -- synchronization up to the SW */
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP			  0x40260000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP			  0x00040000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_0			  0x40260000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_0			  0x00020000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_1			  0x40280000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_1			  0x00020000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SMP			  0x40260000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SMP			  0x00040000
+
+// TRCH <-> RTPS_R52_{LOCKSTEP,SPLIT_{0,1},SMP}
+#define RTPS_DDR_ADDR__SHM__TRCH_SSW__RTPS_R52_LOCKSTEP_SSW 	  0x40260000
+#define RTPS_DDR_SIZE__SHM__TRCH_SSW__RTPS_R52_LOCKSTEP_SSW 	  0x00008000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP_SSW__TRCH_SSW 	  0x40268000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP_SSW__TRCH_SSW 	  0x00008000
+#define RTPS_DDR_ADDR__SHM__TRCH_SSW__RTPS_R52_SPLIT_0_SSW  	  0x40260000
+#define RTPS_DDR_SIZE__SHM__TRCH_SSW__RTPS_R52_SPLIT_0_SSW  	  0x00008000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_0_SSW__TRCH_SSW  	  0x40268000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_0_SSW__TRCH_SSW  	  0x00008000
+#define RTPS_DDR_ADDR__SHM__TRCH_SSW__RTPS_R52_SPLIT_1_SSW  	  0x40260000
+#define RTPS_DDR_SIZE__SHM__TRCH_SSW__RTPS_R52_SPLIT_1_SSW  	  0x00008000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_1_SSW__TRCH_SSW  	  0x40268000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_1_SSW__TRCH_SSW  	  0x00008000
+#define RTPS_DDR_ADDR__SHM__TRCH_SSW__RTPS_R52_SMP_SSW      	  0x40280000
+#define RTPS_DDR_SIZE__SHM__TRCH_SSW__RTPS_R52_SMP_SSW      	  0x00008000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SMP_SSW__TRCH_SSW      	  0x40288000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SMP_SSW__TRCH_SSW      	  0x00008000
+
+// Shared memory regions accessible to RTPS, reserved but not allocated
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_LOCKSTEP__FREE		  0x40280000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_LOCKSTEP__FREE		  0x00020000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_0__FREE		  0x402a0000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_0__FREE		  0x00000000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SPLIT_1__FREE		  0x402a0000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SPLIT_1__FREE		  0x00000000
+#define RTPS_DDR_ADDR__SHM__RTPS_R52_SMP__FREE			  0x40280000
+#define RTPS_DDR_SIZE__SHM__RTPS_R52_SMP__FREE			  0x00020000
 
 //////////
 // HPPS //
 //////////
 
-// Binary images with executables for HPPS
-#define HPPS_DDR_LOW_ADDR__HPPS_SMP   0xc0000000
-#define HPPS_DDR_LOW_SIZE__HPPS_SMP   0x1fc00000 // excludes TRCH reservations in HPPS DRAM
-
 #define WINDOWS_TO_40BIT_ADDR            0xc0000000
 #define WINDOWS_TO_40BIT_SIZE            0x20000000
+
 // Within WINDOWS_TO_40BIT_ADDR, windows to RT_MMU_TEST_DATA_HI_x
 #define RT_MMU_TEST_DATA_HI_0_WIN_ADDR   0xc0000000
 #define RT_MMU_TEST_DATA_HI_1_WIN_ADDR   0xc1000000
+
 // Two regions of same size
 #define RT_MMU_TEST_DATA_HI_0_ADDR      0x100000000
 #define RT_MMU_TEST_DATA_HI_1_ADDR      0x100010000
 #define RT_MMU_TEST_DATA_HI_SIZE            0x10000
 
-// Boot config (TODO: to be replaced by u-boot env/script)
-#define HPPS_DDR_ADDR__HPPS_SMP__BOOT_MODE 0xc001fffc
-
-// Shared memory regions accessible to HPPS
-#define HPPS_DDR_ADDR__SHM__HPPS_SMP 0xc3400000
-#define HPPS_DDR_SIZE__SHM__HPPS_SMP   0x400000
-// HPPS userspace <-> TRCH SSW
-#define HPPS_DDR_ADDR__SHM__HPPS_SMP_APP__TRCH_SSW 0xc3600000
-#define HPPS_DDR_SIZE__SHM__HPPS_SMP_APP__TRCH_SSW    0x10000
-#define HPPS_DDR_ADDR__SHM__TRCH_SSW__HPPS_SMP_APP 0xc3610000
-#define HPPS_DDR_SIZE__SHM__TRCH_SSW__HPPS_SMP_APP    0x10000
-// HPPS SSW <-> TRCH SSW
-#define HPPS_DDR_ADDR__SHM__HPPS_SMP_SSW__TRCH_SSW 0xc39f0000
-#define HPPS_DDR_SIZE__SHM__HPPS_SMP_SSW__TRCH_SSW    0x08000
-#define HPPS_DDR_ADDR__SHM__TRCH_SSW__HPPS_SMP_SSW 0xc39f8000
-#define HPPS_DDR_SIZE__SHM__TRCH_SSW__HPPS_SMP_SSW    0x08000
-
 // Page table should be in memory that is on a bus accessible from the MMUs
 // master port ('dma' prop in MMU node in Qemu DT).  We put it in HPPS DRAM,
 // because that seems to be the only option, judging from high-level Chiplet
 // diagram.
-#define RTPS_HPPS_PT_ADDR                0xc3e00000
+#define RTPS_HPPS_PT_ADDR                0x87e00000
 #define RTPS_HPPS_PT_SIZE                  0x1ff000
 
-#define RT_MMU_TEST_DATA_LO_ADDR         0xc3fff000
-#define RT_MMU_TEST_DATA_LO_SIZE           0x001000
+/* TODO: these have not been verified for memmap v14! might overlap with something! */
+#define RT_MMU_TEST_DATA_LO_0_ADDR	 0x87ffe000
+#define RT_MMU_TEST_DATA_LO_1_ADDR	 0x87fff000
+#define RT_MMU_TEST_DATA_LO_SIZE	 0x00001000
+
+// Full HPPS DRAM
+#define HPPS_DDR_LOW_ADDR__HPPS_SMP				  0x80000000
+#define HPPS_DDR_LOW_SIZE__HPPS_SMP				  0x40000000
+
+// Shared memory regions accessible to HPPS
+#define HPPS_DDR_ADDR__SHM__HPPS_SMP				  0x87600000
+#define HPPS_DDR_SIZE__SHM__HPPS_SMP   				    0x400000
+// HPPS userspace <-> TRCH SSW
+#define HPPS_DDR_ADDR__SHM__HPPS_SMP_APP__TRCH_SSW		  0x87600000
+#define HPPS_DDR_SIZE__SHM__HPPS_SMP_APP__TRCH_SSW 		     0x10000
+#define HPPS_DDR_ADDR__SHM__TRCH_SSW__HPPS_SMP_APP 		  0x87610000
+#define HPPS_DDR_SIZE__SHM__TRCH_SSW__HPPS_SMP_APP 		     0x10000
+// HPPS SSW <-> TRCH SSW
+#define HPPS_DDR_ADDR__SHM__HPPS_SMP_SSW__TRCH_SSW 		  0x879f0000
+#define HPPS_DDR_SIZE__SHM__HPPS_SMP_SSW__TRCH_SSW 		     0x08000
+#define HPPS_DDR_ADDR__SHM__TRCH_SSW__HPPS_SMP_SSW 		  0x879f8000
+#define HPPS_DDR_SIZE__SHM__TRCH_SSW__HPPS_SMP_SSW 		     0x08000
 
 #endif // MEM_MAP_H
--- rtps-r52/tests/lsio-sram.c
+++ rtps-r52/tests/lsio-sram.c
@@ -23,7 +23,7 @@ int test_lsio_sram(void)
 
     test_begin("test_lsio_sram");
 
-    sc = sramfs_init(&sramfs_config, SMC_LSIO_SRAM_BL_FS_START0, NULL);
+    sc = sramfs_init(&sramfs_config, SMC_LSIO_SRAM_BASE0, NULL);
     TEST_SC_OR_SET_RC_GOTO(sc, rc, "LSIO_SRAM: init\n", out);
 
     /* Load a file from "Simple Filesystem" on off-chip memory */
@@ -59,7 +59,7 @@ int test_lsio_sram_dma(void)
     sc = dma_init(&dma_config, BSP_DMA_BASE);
     TEST_SC_OR_SET_RC_GOTO(sc, rc, "LSIO_SRAM_DMA: dma_init\n", out);
 
-    sc = sramfs_init(&sramfs_config, SMC_LSIO_SRAM_BL_FS_START0, &dma_config);
+    sc = sramfs_init(&sramfs_config, SMC_LSIO_SRAM_BASE0, &dma_config);
     TEST_SC_OR_SET_RC_GOTO(sc, rc, "LSIO_SRAM_DMA: sramfs_init\n", uninit_dma);
 
     /* Load a file from "Simple Filesystem" on off-chip memory */
